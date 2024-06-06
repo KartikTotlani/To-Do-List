b0775a82e80adb2f9857f70bd612c39aa07233d9
Kartik Totlani <121310743+KartikTotlani@users.noreply.github.com>
2024-06-06 14:00:57 +0530

    Add files via upload

---
+++ precache-manifest.d80c6a5fd098cbce585ad578d281e7f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d946493348167d6f5596ea12800966d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "498f7e160fe7bd50601b",
+    "url": "./static/css/main.959e16d5.chunk.css"
+  },
+  {
+    "revision": "e52e1da6e8c52863d7a0",
+    "url": "./static/js/2.6c04ec36.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "./static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "498f7e160fe7bd50601b",
+    "url": "./static/js/main.88ce899f.chunk.js"
+  },
+  {
+    "revision": "1f6820902d45c13e132e",
+    "url": "./static/js/runtime~main.b85a6577.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
